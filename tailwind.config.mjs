32e7700e0ee5c5e5fbf12cdeb8f05710664fa34a
danielink <danielcruzzuluaga@gmail.com>
2024-05-29 17:12:58 -0500

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,31 @@
+import animations from "@midudev/tailwind-animations";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#CFCFC5",
+        secondary: "#0D0D0C",
+      },
+      fontFamily: {
+        sans: ["Overused Grotesk"],
+      },
+      animation: {
+        shimmer: "shimmer 2s linear infinite",
+      },
+      keyframes: {
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
+      },
+    },
+  },
+  plugins: [animations],
+};
